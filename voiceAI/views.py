0913aa0d36af94f6bd2fa3039a31daf4453ca6fd
motomario <marius.kaz@icloud.com>
2023-12-15 21:11:24 +0200

    can chat with a AI

--- voiceAI/views.py
+++ voiceAI/views.py
@@ -1,12 +1,11 @@
+import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
-import openai
+from openai import OpenAI
 
-# Make sure to set your OpenAI API key in your environment variables
-# or manage it securely in your Django settings
-openai.api_key = "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"
+client = OpenAI(api_key="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")
 
 def index(request):
     return render(request, 'voiceAI/index.html')
@@ -17,23 +16,14 @@ def process_command(request):
     transcript = request.POST.get('transcript', '')
 
     try:
-        # Update this part to use the new API interface
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # Updated model name for new API
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": transcript},
-            ]
-        )
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+                                                  messages=[{"role": "system", "content": "You are a helpful assistant."},
+                                                            {"role": "user", "content": transcript}])
+        response = json.loads(response.model_dump_json())
+        ai_response = response['choices'][0]['message']['content']
 
-        # Extracting the OpenAI response text
-        ai_response = response['choices'][0]['message']['content']  # Adjusted for new response structure
-
-        # Sending the AI response back to the frontend
         response_data = {'message': ai_response}
-
-    except Exception as e:  # Updated to catch a general exception
-        # Handle errors more specifically if needed
+    except Exception as e:
         response_data = {'error': str(e)}
 
     return JsonResponse(response_data)
