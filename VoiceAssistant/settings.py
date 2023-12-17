a2d984f763b2a7f00d374f2fca377dca48f04c81
motomario <marius.kaz@icloud.com>
2023-12-17 21:24:51 +0200

    AI has a context of current conversation and can reference to previous user messages

--- VoiceAssistant/settings.py
+++ VoiceAssistant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,29 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
--- voiceAI/views.py
+++ voiceAI/views.py
@@ -1,11 +1,16 @@
 import json
+import openai
+import logging
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
-from openai import OpenAI
 
-client = OpenAI(api_key="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")
+# Configure the logger
+logger = logging.getLogger(__name__)
+
+# Instantiate the OpenAI client with your API key
+client = openai.OpenAI(api_key="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")
 
 def index(request):
     return render(request, 'voiceAI/index.html')
@@ -13,17 +18,44 @@ def index(request):
 @csrf_exempt
 @require_http_methods(["POST"])
 def process_command(request):
+    # Retrieve the transcript from the POST data
     transcript = request.POST.get('transcript', '')
 
-    try:
-        response = client.chat.completions.create(model="gpt-3.5-turbo",
-                                                  messages=[{"role": "system", "content": "You are a helpful assistant."},
-                                                            {"role": "user", "content": transcript}])
-        response = json.loads(response.model_dump_json())
-        ai_response = response['choices'][0]['message']['content']
+    # Retrieve the conversation history from the session
+    conversation_history = request.session.get('conversation_history', [])
 
+    logger.debug(f"Received transcript: {transcript}")
+    logger.debug(f"Current conversation history: {conversation_history}")
+
+    try:
+        # Add the user's message to the conversation history
+        conversation_history.append({"role": "user", "content": transcript})
+
+        # Log the updated conversation history
+        logger.debug(f"Updated conversation history: {conversation_history}")
+
+        # Since this is a chat model, use the chat completion endpoint
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=conversation_history
+        )
+
+        # Parse the API response
+        ai_response = response.choices[0].message.content
+
+        # Add the AI's response to the conversation history
+        conversation_history.append({"role": "assistant", "content": ai_response})
+
+        # Save the updated conversation history back to the session
+        request.session['conversation_history'] = conversation_history
+
+        # Prepare the response data
         response_data = {'message': ai_response}
     except Exception as e:
+        # Log the error
+        logger.exception("Error processing the command")
+        # Handle exceptions and prepare the error response
         response_data = {'error': str(e)}
 
+    # Return the AI's response as a JSON
     return JsonResponse(response_data)
