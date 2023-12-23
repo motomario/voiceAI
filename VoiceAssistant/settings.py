0366dafe44a90cdf5111a89a3ebc91837992fd2b
motomario <marius.kaz@icloud.com>
2023-12-23 21:58:30 +0200

    security is done

--- VoiceAssistant/settings.py
+++ VoiceAssistant/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -156,6 +156,6 @@ LOGGING = {
     },
 }
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY').encode('ascii', 'ignore').decode('ascii')
-OPENAI_ASSISTANT_ID = os.getenv('OPENAI_ASSISTANT_ID').encode('ascii', 'ignore').decode('ascii')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '').encode('ascii', 'ignore').decode('ascii')
+OPENAI_ASSISTANT_ID = os.getenv('OPENAI_ASSISTANT_ID', '').encode('ascii', 'ignore').decode('ascii')
 
